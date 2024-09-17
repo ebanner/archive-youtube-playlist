87ac54800d6c81e63e3af3f056f0dd17d6e6e911
Edward Banner <edward.banner@gmail.com>
2024-09-17 10:27:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+
+WATCH_NEXT_PLAYLIST = 'PLkd5S9lUKlOAHYE97mzLKaAIdjhHaXWXS'
+ARCHIVE_PLAYLIST = 'PLkd5S9lUKlOArJsazeSVyZ1syXY2yxdyC'
+
+
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    "client_secrets.json",
+    scopes=[
+        "https://www.googleapis.com/auth/youtube.readonly",
+        "https://www.googleapis.com/auth/youtubepartner",
+        "https://www.googleapis.com/auth/youtube",
+        "https://www.googleapis.com/auth/youtube.force-ssl",
+    ]
+)
+flow.run_local_server(authorization_prompt_message='')
+credentials = flow.credentials
+
+youtube = build('youtube', 'v3', credentials=credentials)
+
+
+def get_watch_next_videos():
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=WATCH_NEXT_PLAYLIST
+    )
+    videos = request.execute()
+    videos = [{'playlist_item_id': video['id'], 'video_id': video['snippet']['resourceId']['videoId']} for video in videos['items']]
+    return videos
+
+
+def add_to_archive(video_id):
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+          "snippet": {
+            "playlistId": ARCHIVE_PLAYLIST,
+            "position": 0,
+            "resourceId": {
+              "kind": "youtube#video",
+              "videoId": video_id
+            }
+          }
+        }
+    )
+    response = request.execute()
+    return response
+
+
+def remove_from_watch_next(playlist_item_id):
+    request = youtube.playlistItems().delete(
+        id=playlist_item_id
+    )
+    response = request.execute()
+    return response
+
+
+if __name__ == '__main__':
+    watch_next_videos = get_watch_next_videos()
+
+    for watch_next_video in watch_next_videos:
+        add_to_archive(watch_next_video['video_id'])
+        remove_from_watch_next(watch_next_video['playlist_item_id'])
